26f2b7fec3cc4a864ab21c307e9cd0c8c060e21a
Jocelin Darma <jocelindarma13@gmail.com>
2023-03-18 22:23:57 +0700

    update css again

--- src/components/Block/Block.jsx
+++ src/components/Block/Block.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Header } from "../Header/Header";
 import { ConnectToGoogle } from "../ConnectToGoogle/ConnectToGoogle";
-import { ExportFile } from "../ExportFile/ExportFile";
+// import { ExportFile } from "../ExportFile/ExportFile";
 import "./Block.css";
 
 export const Block = () => {
   return (
     <div className="block-container">
       <Header />
-      <ExportFile />
-      {/* <ConnectToGoogle /> */}
+      {/* <ExportFile /> */}
+      <ConnectToGoogle />
     </div>
   );
 };
--- src/components/ExportFile/ExportFile.jsx
+++ src/components/ExportFile/ExportFile.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ChevronDown from "../../assets/chevron-down.svg"
+import GSheetsIcon from "../../assets/sheets-icon.svg";
+import CancelIcon from "../../assets/cancel-icon.svg";
 import "./ExportFile.css";
 
 export const ExportFile = () => {
@@ -14,6 +16,38 @@ export const ExportFile = () => {
             Account Name
           </div>
           <img className="icons-1" src={ChevronDown} alt="Chevron Down"/>
+        </div>
+      </div>
+      <div className='file'>
+        <div className='file-text figtree-semi-bold-shark-11px'>
+          File
+        </div>
+        <div className='file-selector'>
+          <div className='sheetname'>
+            <img className='icons-1' src={GSheetsIcon} alt="GSheets"/>
+            <div className='sheetname-text figtree-medium-shark-11px'>
+              sheetname
+            </div>
+          </div>
+          <div className='tab-component'>
+            <div className='tab'>
+              <div className='tab-1 figtree-semi-bold-gunsmoke-10px'>
+                Tab 1
+              </div>
+              <img className='icons' src={ChevronDown} alt="Chevron Down"/>
+            </div>
+            <img className='icons-1' src={CancelIcon} alt="Cancel"/>
+          </div>
+        </div>
+      </div>
+      <div className='export'>
+        <div className='export-button'>
+          <div className='export-text figtree-semi-bold-white-11px'>
+            Export
+          </div>
+        </div>
+        <div className='last-export figtree-medium-gunsmoke-10px'>
+          Last export 14h ago
         </div>
       </div>
     </div>
